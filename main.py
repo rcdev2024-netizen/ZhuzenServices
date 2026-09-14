7dc1e8ef04728011cd7c2924b0bbcc1536d453ef
rcdev <rcdev2024@gmail.com>
2026-09-14 17:24:17 +0800

    g
    g

--- main.py
+++ main.py
@@ -27,11 +27,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://servoces-ops-chi.vercel.app",
-        "http://localhost:5173",
-        "http://localhost:3000",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
